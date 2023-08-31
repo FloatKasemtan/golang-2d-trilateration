76c3681e5b2e6d6e6cc666a23a7716e2c190235c
Floaty_KT <69688279+FloatKasemtan@users.noreply.github.com>
2023-08-31 20:33:25 +0700

    Add height calculation

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ import (
 )
 
 func main() {
-	pos1 := models.NewPosition(10000, 2000)
-	pos2 := models.NewPosition(20000, 2300)
-	pos3 := models.NewPosition(14000, 2100)
-	pos1.SetRadius(1000)
-	pos2.SetRadius(2000)
-	pos3.SetRadius(1500)
+	pos1 := models.NewPositionWithHeight(10000, 2000, 100)
+	pos2 := models.NewPositionWithHeight(20000, 2300, 100)
+	pos3 := models.NewPositionWithHeight(14000, 2100, 150)
+	if err := pos1.SetRadius(1000); err != nil {
+		panic(err)
+	}
+	if err := pos2.SetRadius(2000); err != nil {
+		panic(err)
+	}
+	if err := pos3.SetRadius(1500); err != nil {
+		panic(err)
+	}
 	sys := models.NewTrilateration([]*models.Position{pos1, pos2, pos3})
 	pos, _ := sys.GetPosition()
 	fmt.Println(pos)
--- trilateration/models/Position.go
+++ trilateration/models/Position.go
@@ -1,9 +1,17 @@
 package models
 
+import (
+	"errors"
+	"math"
+)
+
+const AvgTagHeight = 100.0
+
 type Position struct {
-	X float64
-	Y float64
-	R float64
+	X      float64
+	Y      float64
+	Height float64
+	R      float64
 }
 
 func NewPosition(x float64, y float64) *Position {
@@ -13,8 +21,21 @@ func NewPosition(x float64, y float64) *Position {
 	}
 }
 
-func (p *Position) SetRadius(r float64) {
+func NewPositionWithHeight(x float64, y float64, height float64) *Position {
+	return &Position{
+		X:      x,
+		Y:      y,
+		Height: height,
+	}
+}
+
+func (p *Position) SetRadius(distance float64) error {
+	r := math.Sqrt(math.Abs(math.Pow(distance, 2) - math.Pow(p.Height-AvgTagHeight, 2)))
+	if math.IsNaN(r) {
+		return errors.New("invalid radius")
+	}
 	p.R = r
+	return nil
 }
 
 type ByRadius []*Position
